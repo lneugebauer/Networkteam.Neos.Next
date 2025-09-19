0315c9415a4f264e1fddabda0351bef0a85322d1
Jonathan Philipp <jonathan.philipp@networkteam.com>
2025-09-19 15:02:46 +0200

    feat: dispatch Event on reloadOutOfBand

--- Resources/Public/NeosUserInterface/Plugin.js
+++ Resources/Public/NeosUserInterface/Plugin.js
@@ -15,11 +15,19 @@
   // src/manifest.js
   dist_default("Networkteam.Neos.Next", {}, (globalRegistry) => {
     const saveHooksRegistry = globalRegistry.get("inspector").get("saveHooks");
-    console.log("Trying to register custom Registry Hook");
+    const serverFeedbackHandlers = globalRegistry.get("serverFeedbackHandlers");
     saveHooksRegistry.set("Networkteam.Neos.Next:Hook.BeforeSave", (oldValue, options) => {
-      console.log(`value:`, value);
+      console.log(`value:`, oldValue);
       console.log("options:", options);
       return oldValue;
+    });
+    serverFeedbackHandlers.set("Neos.Neos.Ui:ReloadContentOutOfBand/Main", (feedback, store) => {
+      console.log("reloadContentOufOfBand");
+      const guestFrame = document.getElementsByName("neos-content-main")[0];
+      console.log(guestFrame);
+      const event = new Event("Networkteam.Neos.Next:ReloadOutOfBand");
+      console.log(event);
+      guestFrame.contentWindow.dispatchEvent(event);
     });
   });
 })();
