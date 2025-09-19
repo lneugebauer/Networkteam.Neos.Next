0ca7463bea53544d4ed204f341ea821a3d68f74e
Jonathan Philipp <jonathan.philipp@networkteam.com>
2025-09-19 15:24:04 +0200

    chore: cleanup unused code for event dispatcher

--- Resources/Private/NeosUserInterface/src/manifest.js
+++ Resources/Private/NeosUserInterface/src/manifest.js
@@ -1,20 +1,11 @@
 import manifest from '@neos-project/neos-ui-extensibility';
 
 manifest('Networkteam.Neos.Next', {}, (globalRegistry) => {
-  const saveHooksRegistry = globalRegistry.get('inspector').get('saveHooks')
   const serverFeedbackHandlers = globalRegistry.get('serverFeedbackHandlers')
-  saveHooksRegistry.set('Networkteam.Neos.Next:Hook.BeforeSave', (oldValue, options) => {
-    console.log(`value:`, oldValue);
-    console.log('options:', options);
-    return oldValue;
-  })
-
+  
   serverFeedbackHandlers.set('Neos.Neos.Ui:ReloadContentOutOfBand/Main', (feedback, store) => {
-    console.log('reloadContentOufOfBand');
     const guestFrame = document.getElementsByName('neos-content-main')[0];
-    console.log(guestFrame);
-    const event = new Event('Networkteam.Neos.Next:ReloadOutOfBand');
-    console.log(event)
-    guestFrame.contentWindow.dispatchEvent(event);
+    const reloadOutOfBandEvent = new Event('Networkteam.Neos.Next:ReloadOutOfBand');
+    guestFrame.contentWindow.dispatchEvent(reloadOutOfBandEvent);
   })
 });
